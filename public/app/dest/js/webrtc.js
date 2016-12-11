71ad0c0697a6644e822662e064dff15c4a177bd4
lduoduo <sdlina317@gmail.com>
2016-12-11 17:37:03 +0800

    init

--- public/app/dest/js/webrtc.js
+++ public/app/dest/js/webrtc.js
@@ -4,40 +4,98 @@ navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia;
+
+window.AudioContext = window.AudioContext || webkitAudioContext;
+
+var actx = new AudioContext();
+var canvas = document.createElement('canvas');
+canvas.width = 320;canvas.height = 240;
+var cctx = canvas.getContext('2d');
+var video = document.querySelector('#rtc');
+
 var constraints = {
-    video: true,
-    audio: true
+    video: {
+        mandatory: {
+            maxWidth: 320,
+            maxHeight: 240
+        }
+    },
+    audio: false
 };
 
-if (navigator.getUserMedia) {
-    // 支持
-    var um = navigator.getUserMedia(constraints, success, err);
-} else {
-    console.log('web rtc not supported');
-    // 不支持
-}
+var rtc = {
+    init() {
+        this.initStatus();
+        this.initEvent();
+    },
+    initEvent() {
+        var _this = this;
+        var shots = $('#snapshot');
+        shots.addEventListener('click',(e) => {
+            console.log(e);
+            if(_this.stream){
+                cctx.drawImage(video,0,0);
+                $('#img').src = canvas.toDataURL('image/webp');
+            }
+        });
+    },
+    initStatus() {
+        var _this = this;
+        if (navigator.getUserMedia) {
+            // 支持
+            navigator.getUserMedia(constraints, function(stream){
+                _this.loadStream(_this, stream);
+            }, _this.err);
+            _this.mstlist();
+        } else {
+            console.log('web rtc not supported');
+            // 不支持
+        }
+    },
+    loadStream(ctx,stream) {
+        console.log(stream);
+        ctx.stream = stream;
+        // var video = document.createElement('video');
+        
 
-function success(stream) {
-    console.log(stream);
-    // var video = document.createElement('video');
-    var video = document.querySelector('#rtc');
-    if (window.URL) {
-        video.src = window.URL.createObjectURL(stream);
-    } else {
-        video.src = stream;
+        if (window.URL) {
+            video.src = window.URL.createObjectURL(stream);
+        } else {
+            video.src = stream;
+        }
+        // video.autoplay = true;
+        //or
+        video.play();
+
+        video.onloadedmetadata = function(e) {
+            console.log("Label: " + stream.label);
+            console.log("AudioTracks", stream.getAudioTracks());
+            console.log("VideoTracks", stream.getVideoTracks());
+        };
+
+        // var audioInput = actx.createMediaStreamSource(stream);
+        // audioInput.connect(actx.destination);
+
+    },
+    err(e) {
+        console.log(e);
+    },
+    //获取摄像头和麦克风的列表
+    mstlist() {
+        MediaStreamTrack.getSources(function(sourceInfo) {
+            var audioSource = null;
+            var videoSource = null;
+            console.log(sourceInfo);
+            // for( let i=0;i!=sourceInfo.length;i++){
+
+            // }
+        });
     }
-    video.autoplay = true;
-    //or
-    // video.play();
 
-    video.onloadedmetadata = function (e) {
-        console.log("Label: " + stream.label);
-        console.log("AudioTracks", stream.getAudioTracks());
-        console.log("VideoTracks", stream.getVideoTracks());
-    };
-
-    document.body.appendChild(video);
 }
-function err(e) {
-    console.log('err:' + e);
+
+rtc.init();
+
+function $(name){
+    return document.querySelector( name);
 }
