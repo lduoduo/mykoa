4424f8a7928b3f133c53a75cdf1b24c0c625e6bb
lduoduo <sdlina317@gmail.com>
2016-12-17 15:21:52 +0800

    1

--- config/config.dev.js
+++ config/config.dev.js
@@ -10,7 +10,7 @@ module.exports = class Config {
         /*当前站点环境*/
         this.env = 'dev';
         /** 本地ip */
-        this.ip = getLocalIps()[0];
+        this.ip = getLocalIps()[1];
         /*站点的端口*/
         this.serverPort = 8080;
         /*站点的https端口*/
@@ -20,11 +20,10 @@ module.exports = class Config {
         /*本地静态资源的端口*/
         this.staticPorts = 8091;
         /*站点引用静态资源的地址*/
-        this.frontURL =
-            {
-                js: this.ip + this.staticPort + '/dest/js/',
-                css: this.ip + this.staticPort + '/dest/css/'
-            };
+        this.frontURL = {
+            js: 'https://' + this.ip + ':' + this.staticPorts + '/js/',
+            css: 'https://' + this.ip + ':' + this.staticPorts + '/css/'
+        };
         /*域名*/
         this.domain = 'gooddogdesign.com';
         /*路由虚拟目录*/
@@ -33,7 +32,7 @@ module.exports = class Config {
         // this.interUrl = "http://10.14.91.132:8090/nodeapi/";
         this.interUrl = 'http://' + this.ip + ':9998/nodeapi/';
         // this.monitorUrl = '//' + this.ip + ':9999/updateLog';
-        this.monitorUrl  = '//' + this.ip + ':9998/';
+        this.monitorUrl = '//' + this.ip + ':9998/';
     }
 }
 
@@ -41,7 +40,7 @@ module.exports = class Config {
 function getLocalIps(flagIpv6) {
     var ifaces = os.networkInterfaces();
     var ips = [];
-    var func = function (details) {
+    var func = function(details) {
         if (!flagIpv6 && details.family === 'IPv6') {
             return;
         }
