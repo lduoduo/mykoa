793a664bf05f0f4237b8a917b3a89074e1b03450
lduoduo <sdlina317@gmail.com>
2016-12-26 00:07:59 +0800

    11

---
+++ public/app/dest/js/canvas.js
@@ -0,0 +1,195 @@
+/** canvas demo for AR  */
+
+//弹窗插件配置
+var Mt = {
+    alert: function (option) {
+        //type, title, msg, btnMsg, cb, isLoading
+        swal({
+            title: option.title,
+            text: option.msg,
+            type: option.type,
+            showConfirmButton: !!option.confirmBtnMsg,
+            showCancelButton: !!option.cancelBtnMsg,
+            cancelButtonText: option.cancelBtnMsg || "在犹豫一下",
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: option.confirmBtnMsg || "好哒",
+            showLoaderOnConfirm: option.isLoading,
+            timer: option.timer,
+            closeOnConfirm: false,
+            html: option.html
+        }, option.cb);
+    },
+    close: function () {
+        swal.close();
+    }
+};
+
+var params = {
+    fileInput: $('.J-upload')[0],
+    // dragDrop: $("#drag")[0],
+    // upButton: $("#uploadBtn")[0],
+    url: '/upload',
+    //list为已经上传的文件
+    filter: function (files, listobj) {
+        var file = files[0];
+        var errors = '';
+
+        if (!/\.(png|jpeg|jpg)$/i.test(file.name)) {
+            errors = "只能上传图片格式的文件";
+            files = [];
+        }
+
+        if (errors) {
+            Mt.alert({
+                title: '呃。。。！',
+                type: 'error',
+                msg: errors,
+                confirmBtnMsg: '好'
+            });
+        }
+        return {
+            list: files,
+            obj: listobj
+        }
+    },
+    onSelect: function (files) {
+        files = files[0];
+
+        if (files) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                canvas.draw(e.target.result);
+            }
+            reader.readAsDataURL(files);
+        }
+
+    },
+    onError: function (msg) {
+        Mt.alert({
+            type: 'error',
+            title: msg
+        });
+    }
+};
+ZXXFILE = $.extend(ZXXFILE, params);
+ZXXFILE.init();
+
+var canvas = {
+    canvas: null, //绘图canvas
+    ctx: null, //绘图上下文
+    image: null, //图片Image实例
+    init: function () {
+        this.canvas = $('.canvas')[0];
+        // this.canvas.width = document.body.clientWidth;
+        // this.canvas.height = document.body.clientHeight * 0.9;
+        this.ctx = this.canvas.getContext('2d');
+        this.initEvent();
+        this.initHammer();
+    },
+    initEvent: function () {
+        $('body').on('click', '.J-btn-upload', function () {
+            $('.J-upload').click();
+        });
+        // $('.J-upload').on('change', function (e) {
+
+        // });
+    },
+    //初始化手势监控
+    initHammer: function () {
+        var target = document.querySelector('.canvas');
+        target.style.webkitTransition = 'all ease 0.05s';
+
+        touch.on('.canvas', 'touchstart', function (ev) {
+            ev.preventDefault();
+        });
+
+        var initialScale = 1;
+        var currentScale;
+        v ==1;
+
+        touch.on('.canvas', 'pinchstart', function (ev) {
+            currentScale = ev.scale - 1;
+            currentScale = initialScale + currentScale;
+            currentScale = currentScale > 2 ? 2 : currentScale;
+            currentScale = currentScale < 1 ? 1 : currentScale;
+            this.style.webkitTransform = 'scale(' + currentScale + ')';
+
+            ERROR.logtype = "touch";
+            ERROR.log = "当前缩放比例为:" + currentScale + ".";
+            // alert(JSON.stringify(perf));
+            ajax.post('/data/updateLog', ERROR);
+
+            // log("当前缩放比例为:" + currentScale + ".");
+        });
+
+        touch.on('.canvas', 'pinchend', function (ev) {
+            initialScale = currentScale;
+        });
+
+    },
+    //加载图片获取图片数据
+    loadImage: function (data) {
+        var _ = this;
+        if (!_.image) {
+            _.image = new Image();
+        }
+        var image = _.image;
+        image.onload = function () {
+            _.draw();
+        }
+
+        image.src = data;
+    },
+    //使用canvas进行绘图
+    draw: function (scale) {
+
+        _.ctx.clearRect(0, 0, _.canvas.width, _.canvas.height);
+        _.ctx.beginPath();
+
+        var tmp = {};
+        var image = _.image;
+        var co = _.canvas;
+        var w = image.width, h = image.height;
+
+        co.width = document.body.clientWidth;
+        co.height = document.body.clientHeight;
+
+        //按照长宽比等比例自适应屏幕
+        if (image.width != 0) {
+            var scale1 = image.width / image.height;
+            var scale2 = co.width / co.height;
+            if (scale1 >= scale2) {
+                co.height = image.height * co.width / image.width;
+                image.style.width = '100%';
+                image.style.height = 'auto';
+                co.style.marginLeft = '-' + co.width / 2 + 'px';
+                co.style.marginTop = '-' + co.height / 2 + 'px';
+            }
+            else {
+                co.width = image.width * co.height / image.height;
+                image.style.height = '100%';
+                image.style.width = 'auto';
+                co.style.marginLeft = '-' + co.width / 2 + 'px';
+                co.style.marginTop = '-' + co.height / 2 + 'px';
+            }
+
+            //带缩放系数的画图
+            if (scale) {
+                tmp.w = co.width * scale;
+                tmp.h = co.height * scale;
+                tmp.dx = (co.width - tmp.w) * 0.5;
+                tmp.dy = (co.height - tmp.h) * 0.5;
+                _.ctx.drawImage(image, tmp.dx, tmp.dy, tmp.w, tmp.h);
+
+                return;
+            }
+
+            _.ctx.drawImage(image, 0, 0, _.canvas.width, _.canvas.height);
+        }
+    }
+
+}
+
+canvas.init();
+
+
--- public/app/dest/js/common.js
+++ public/app/dest/js/common.js
@@ -1,1 +1,150 @@
 FastClick.attach(document.body);
+
+/*
+ * zxxFile.js 基于HTML5 文件上传的核心脚本 http://www.zhangxinxu.com/wordpress/?p=1923
+ * by zhangxinxu 2011-09-12
+ */
+
+var ZXXFILE = {
+    fileInput: null, //html file控件
+    dragDrop: null, //拖拽敏感区域
+    upButton: null, //提交按钮
+    isMultiple: false, // 是否多文件上传
+    url: "", //ajax地址
+    fileFilter: [], //过滤后的文件数组
+    fileFilterObj: {}, //过滤后的文件对象集合，用户去重过滤
+    filter: function (files) { //选择文件组的过滤方法
+        return files;
+    },
+    onSelect: function () { }, //文件选择后
+    onDelete: function () { }, //文件删除后
+    onDragOver: function () { }, //文件拖拽到敏感区域时
+    onDragLeave: function () { }, //文件离开到敏感区域时
+    beforeUpload: function () { }, //文件上传之前的验证
+    onProgress: function () { }, //文件上传进度
+    onSuccess: function () { }, //文件上传成功时
+    onFailure: function () { }, //文件上传失败时,
+    onComplete: function () { }, //文件全部上传完毕时
+
+    /* 开发参数和内置方法分界线 */
+
+    //文件拖放
+    funDragHover: function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this[e.type === "dragover" ? "onDragOver" : "onDragLeave"].call(e.target);
+        return this;
+    },
+    //获取选择文件，file控件或拖放
+    funGetFiles: function (e) {
+        // 取消鼠标经过样式
+        this.funDragHover(e);
+
+        // 获取文件列表对象
+        var files = e.target.files || e.dataTransfer.files;
+        //继续添加文件
+        var tmp = this.filter(files, this.fileFilterObj);
+
+        this.fileFilter = this.isMultiple ? this.fileFilter.concat(tmp.list) : tmp.list;
+        this.fileFilterObj = tmp.obj;
+        tmp.list.length > 0 &&  this.funDealFiles();
+        return this;
+    },
+
+    //选中文件的处理与回调
+    funDealFiles: function () {
+        for (var i = 0, file; file = this.fileFilter[i]; i++) {
+            //增加唯一索引值
+            file.index = i;
+        }
+        //执行选择回调
+        this.onSelect(this.fileFilter);
+        return this;
+    },
+
+    //删除对应的文件
+    funDeleteFile: function (name) {
+        for (var i = 0; i < this.fileFilter.length; i++) {
+            if (this.fileFilter[i].name == name) {
+                index = i;
+                this.fileFilter.splice(i, 1);
+                break;
+            }
+        }
+        delete this.fileFilterObj[name];
+        this.onDelete(name);
+    },
+
+    //文件上传
+    funUploadFile: function () {
+        var self = this;
+        if (location.host.indexOf("sitepointstatic") >= 0) {
+            //非站点服务器上运行
+            return;
+        }
+        if (!this.beforeUpload(this.fileFilterObj)) {
+            return;
+        }
+        this.ajaxFile(this.fileFilter);
+    },
+
+    init: function () {
+        var self = this;
+
+        if (this.dragDrop) {
+            this.dragDrop.addEventListener("dragover", function (e) {
+                self.funDragHover(e);
+            }, false);
+            this.dragDrop.addEventListener("dragleave", function (e) {
+                self.funDragHover(e);
+            }, false);
+            this.dragDrop.addEventListener("drop", function (e) {
+                self.funGetFiles(e);
+            }, false);
+        }
+
+        //文件选择控件选择
+        if (this.fileInput) {
+            this.fileInput.addEventListener("change", function (e) {
+                self.funGetFiles(e);
+            }, false);
+        }
+
+        //上传按钮提交
+        if (this.upButton) {
+            this.upButton.addEventListener("click", function (e) {
+                self.funUploadFile(e);
+            }, false);
+        }
+    },
+    ajaxFile: function (files) {
+        if ($('#projectname').val() == '') {
+            this.onError('项目名不能为空');
+            return false;
+        }
+        var _this = this;
+        var fd = new FormData();
+        fd.append('name', $('#projectname').val() || 'test');
+        for (var i = 0; i < files.length; i++) {
+            fd.append('file', files[i]);
+        }
+        // fd.append('file', $('#multipleUpload')[0].files);
+
+        $.ajax({
+            url: _this.url,
+            type: 'POST',
+            data: fd,
+            processData: false, //此处指定对上传数据不做默认的读取字符串的操作
+            contentType: false, //此处指定对上传数据不做默认的读取字符串的操作
+            success: function (data) {
+                _this.fileFilter = [];
+                _this.fileFilterObj = {};
+                _this.onSuccess(data);
+                // console.log(data);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }
+};
